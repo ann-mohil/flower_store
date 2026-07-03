1e554dc84eb5e04124785702808abbd2e745b6a7
ann-mohil <anna.mohil.25@kse.org.ua>
2026-07-04 02:51:26 +0300

    testing

---
+++ tested explain analyze.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE VIEW all_orders AS
+SELECT
+    o.order_id,
+    o.order_date,
+    o.status,
+    o.total_amount,
+    c.full_name AS customer_name,
+    c.email AS customer_email,
+    b.bouquet_name,
+    oi.quantity,
+    oi.unit_price,
+    (oi.quantity * oi.unit_price) AS line_total
+FROM order_items oi
+JOIN orders o ON oi.order_id = o.order_id
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN bouquets b ON oi.bouquet_id = b.bouquet_id;
+
+DROP INDEX idx_order_items_order_id;
+EXPLAIN ANALYZE SELECT * FROM order_items WHERE order_id = 19999;
+
+CREATE INDEX idx_order_items_order_id ON order_items(order_id);
+EXPLAIN ANALYZE SELECT * FROM order_items WHERE order_id = 19999;
